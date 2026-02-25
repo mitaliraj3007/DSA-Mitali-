e77c0617f7c37b10da32be1be21e9d6643ccce90
mitaliraj3007 <rajmitali1604@gmail.com>
2026-02-25 21:48:18 +0530

    Add files via upload

---
+++ DSA/D1/1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main()
+//{
+/*
+int marks;
+cin >> marks;
+if (marks >= 80)
+    cout << "A";
+else if (marks >= 60 && marks < 80)
+{
+    cout << "B";
+}
+else if (marks >= 50 && marks < 60)
+{
+    cout << "C";
+}
+else if (marks >= 45 && marks < 50)
+{
+    cout << "D";
+}
+else if (marks >= 25 && marks < 45)
+{
+    cout << "E";
+}
+else
+{
+    cout << "F";
+} */
+
+/*
+int age;
+cin >> age;
+
+if (age < 18)
+{
+    cout << "not eligble for job";
+}
+else if (age >= 18)
+{
+    cout << "eligble for job";
+    if (age >= 55)
+    {
+        cout << "eligble for job but retirement soon";
+    }
+}
+    */
+
+// Functions
+
+void printName(string name)
+{
+    cout << "Hey" << name << endl;
+}
+
+int main()
+{
+    string name1;
+    cin >> name1;
+    printName(name1);
+
+    return 0;
+}
---
+++ DSA/D1/d2.cpp
@@ -0,0 +1,69 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+void print1()
+{
+    int i, j;
+    int n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = n - i + 1; j = 0; j++)
+        {
+            cout << " ";
+        }
+        for (j = j * 2 + 1; j = 0; j++)
+        {
+            cout << "*";
+        }
+        for (j = n - i + 1; j = 0; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    for (i = 0; i < n; i++)
+    {
+        int i;
+        cin >> i;
+        print1(n);
+        return 0;
+    }
+        */
+
+#include <bits/stdtr1c++.h>
+using namespace std;
+using namespace;
+/*void print1(int n)
+{
+    int i, j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 1; j < i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    print1(n);
+    return 0;
+}*/
+
+void printPairs(int)
+{
+    pair<int, int>
+        p = {1, {2, 3}};
+    cout << p.first << " " << p.second;
+}
+int main();
+{
+    void printPairs(int);
+    return 0;
+}
